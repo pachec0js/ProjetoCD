8d5e895efb5f641fef6569993c10e740bea0f334
BRUNO PEZZOLATO HOLLOSI <47810695843@info.local>
2026-03-04 21:29:47 -0300

    Numero de euler - Bruno

--- Atividade/SenaiMath.py
+++ Atividade/SenaiMath.py
@@ -1,1 +1,17 @@
 pi = 3.141592653589793
+
+
+def numeroeuler(n):
+    total = 0
+
+    for pedaco in range(n):
+        fatorialAtual = 1
+        for i in range(1, pedaco + 1):
+            fatorialAtual *= i
+        expressao = 1 / fatorialAtual
+        total += expressao
+
+    return total
+
+
+print(numeroeuler(100))
