7d68668c464c025aa84ca2b4d039ba2454354b6d
pachec0js <fabio.h.l.pacheco@gmail.com>
2026-03-04 22:09:57 -0300

    Função Cosseno

--- Atividade/SenaiMath.py
+++ Atividade/SenaiMath.py
@@ -1,3 +1,26 @@
+pi = 3.141592653589793
+k = 100
+
+def radiano(x):
+    return x * pi / 180
+
+
+def cosseno(x):
+    cos = 0
+    
+    for pedaco in range(k):
+        fatorialAtual = 2 * pedaco
+        
+        termo = (-1) ** pedaco
+        
+        for i in range(1, fatorialAtual + 1):
+            termo = termo * x / i
+        
+        cos += termo
+
+    return cos
+
+
 def exponencial(x, k=100):
     resultado = 1.0 
     termo = 1.0
