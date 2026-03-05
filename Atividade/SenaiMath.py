073ff7881cb487517309cdc085151289a0b9a657
ARTHUR BUSCARINO BENEDETTI <45307470851@info.local>
2026-03-04 21:24:38 -0300

    Fiz o exponencial de um numero

--- Atividade/SenaiMath.py
+++ Atividade/SenaiMath.py
@@ -1,1 +1,13 @@
-pi = 3.141592653589793
+numero = float(input("Digite o numero para saber o exponencial: "))
+
+def exponencial(x, k=100):
+    resultado = 1.0 
+    termo = 1.0
+    
+    for n in range(1, k):
+        termo *= x / n   
+        resultado += termo
+    
+    return resultado
+
+print(f"e^{numero} = {exponencial(numero):.6f}")
